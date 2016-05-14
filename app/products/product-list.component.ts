2f93d3aad36a32404cc80026bbffbd3eda5c2eb9
Noel Austin <Noel Austin>
2016-05-14 13:37:04 -0400

    added ngIf for product table and added products property and loaded with two items for Product Item component

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -8,6 +8,31 @@ import { Component } from 'angular2/core'
    * ProductListComponent
    */
   class ProductListComponent {
-  
+     pageTitle: string = "Product List";
+     products: any[] = [
+       {
+         "productId": 2,
+         "productName": "Garden Cart",
+         "productCode": "GDN-0023",
+         "releaseDAte": "March 18, 2016",
+         "description": "15 gallon capacity",
+         "price": 32.99,
+         "starRating": 4.2,
+         "imageURL": "http://openclipart.org"
+         
+       },
+       {
+         "productId": 3,
+         "productName": "Hammer",
+         "productCode": "GDN-0024",
+         "releaseDAte": "March 19, 2016",
+         "description": "Curved claw steel hammer",
+         "price": 8.9,
+         "starRating": 4.8,
+         "imageURL": "http://openclipart.org"
+         
+       }
+       
+     ];
   }
   
